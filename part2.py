06595fdeab23ba99a81b0339e6d950e9c60b1e87
skykanin <3789764+skykanin@users.noreply.github.com>
2019-03-21 21:21:39 +0100

    More progress on part2

--- part2.py
+++ part2.py
@@ -1,15 +1,13 @@
 '''This module does data aggregation using spark on tweets'''
 import sys
-import tempfile
 from functools import reduce
 from pyspark import SparkConf, SparkContext
-from typing import List
 
 CONF = SparkConf() \
   .setAppName("main") \
   .setMaster("local[*]") \
- # .setExecutorEnv('spark.driver.memory', '14g') \
- # .setExecutorEnv('spark.executor.memory', '14g')
+  .setExecutorEnv('spark.driver.memory', '5g') \
+  .setExecutorEnv('spark.executor.memory', '3g')
 SC = SparkContext(conf=CONF)
 
 TWITTER_HEADERS = ["user", "tweet_text"]
@@ -41,10 +39,16 @@ def freq(word, tweet_words):
 def sim_score(tweets_x, tweets_y):
     '''Calculates the similarity score betweeen two users'''
     word_set = set().union(tweets_x, tweets_y)
-    return reduce(
+    my_sum = 0
+    for word in word_set:
+        my_sum += min(freq(word, tweets_x), freq(word, tweets_y))
+
+    return my_sum
+
+    '''return reduce(
         lambda a, b: min(freq(a, tweets_x), freq(a, tweets_y))
         + min(freq(b, tweets_x), freq(b, tweets_y))
-        , word_set)
+        , word_set)'''
 
 def get_user_words(rdd, user):
     '''Return list of all distinct user words from tweets'''
@@ -55,7 +59,7 @@ def get_user_words(rdd, user):
         return(step
                .values()
                .flatMap(lambda val: val.split()) #[user_words]
-               .distinct()
+               #.distinct()
                .collect())
 
 def get_union_words(rdd, queried_user_words, other_user):
@@ -63,12 +67,12 @@ def get_union_words(rdd, queried_user_words, other_user):
 
 # rdd = SC.parallelize([("meme", "he she tree lee fee ree"), ("nope", "top fope lee ree never ever"), ("scope", "tope rope lee tree she"), ("nope", "never ever lever top he")])
 
-def sim_usrs(rdd, user="tmj_bos_hr", k=3):
-    '''Test'''
+def rmd_usrs(rdd, user="tmj_bos_hr", k=3):
+    '''Returns list of recommended users'''
     tweet_words = intern_rdd(rdd, user)
-    #queried_user_words = get_user_words(rdd, user)
-    #union_rows = union_rdd(rdd, user)
-    
+    union_rows = union_rdd(rdd, user)
+    queried_user_words = get_user_words(rdd, user)
+
     return(rdd
            .filter(lambda row: not row[0] == user)
            .mapValues(lambda s: s.split()) #(user, [tweet_words])
@@ -76,24 +80,40 @@ def sim_usrs(rdd, user="tmj_bos_hr", k=3):
            .flatMapValues(identity) #(user, word)
            .distinct()
            .join(tweet_words) #(user, (word, [tweet_words]))
-           #.join(union_rows) #(user, ((word, [tweet_words]), {tweet_words_union_set}))
-           #.reduceByKey(lambda a, b:
-                       # min(freq(a[0][0], a[0][1]), freq(a[0][0], queried_user_words))
-                       # + min(freq(b[0][0], b[0][1]), freq(b[0][0], queried_user_words)))
-           .coalesce(1)
-           .saveAsTextFile("result/tweets"))
+           .join(union_rows) #(user, ((word, [tweet_words]), {tweet_words_union_set}))
+           .reduceByKey(lambda a, b:
+                        min(freq(a[0][0], a[0][1]), freq(a[0][0], queried_user_words))
+                        + min(freq(b[0][0], b[0][1]), freq(b[0][0], queried_user_words)))
+           .take(k))
+           #.coalesce(1)
+           #.saveAsTextFile("result/tweets"))
+
+
+def rmd_usrs2(rdd, user="tmj_bos_hr", k=3):
+    '''Returns list of recommended users'''
+
+    queried_user_tweets = get_user_words(rdd, user) #[queried_user_tweet_words]
+
+    return(rdd
+           .filter(lambda row: not row[0] == user)
+           .mapValues(lambda s: s.split())
+           .reduceByKey(lambda a, b: a + b) #(user, [tweet_words])
+           .mapValues(lambda l: sim_score(queried_user_tweets, l))
+           .sortByKey()
+           .takeOrdered(k, key=lambda t: t[1]))
 
 def intern_rdd(rdd, user):
+    '''Return touple of user and list of tweet words'''
     return(rdd
            .filter(lambda row: not row[0] == user)
            .mapValues(lambda s: s.split()) #(user, [tweet_words])
            .reduceByKey(lambda a, b: a + b)) #(user, [tweet_words]))
 
-    
+
 def union_rdd(rdd, user):
     '''Return rdd containing touples of user and union words for queried user'''
     user_words = get_user_words(rdd, user)
-    
+
     return(rdd
            .filter(lambda row: not row[0] == user)
            .mapValues(lambda s: s.split())
@@ -102,7 +122,7 @@ def union_rdd(rdd, user):
            .reduceByKey(lambda a, b: a + ' ' + b) #(user, tweet_words_string)
            .mapValues(lambda l: l.split() + user_words)) #(user, [tweet_words_union_set])
 
-def similar_users(user, k, file_path, output_file):
+def recommended_users(user, k, file_path, output_file):
     '''Writes similar users to file'''
     conf = SparkConf().setAppName("main").setMaster("local[*]")
     sc = SparkContext(conf=conf)
@@ -112,6 +132,7 @@ def similar_users(user, k, file_path, output_file):
 
 
 def main(argv):
+    '''Main function'''
     print(argv)
 
 if __name__ == "__main__":
