a98e46c41c33987b98a0be438152c01a15e2c078
skykanin <3789764+skykanin@users.noreply.github.com>
2019-03-21 17:48:30 +0100

    Updated part 2

--- part2.py
+++ part2.py
@@ -1,7 +1,9 @@
 '''This module does data aggregation using spark on tweets'''
 import sys
+import tempfile
 from functools import reduce
 from pyspark import SparkConf, SparkContext
+from typing import List
 
 CONF = SparkConf() \
   .setAppName("main") \
@@ -31,32 +33,14 @@ def identity(a):
     '''Returns itself'''
     return a
 
-def intersperse(iterable, delimiter):
-    '''Intersperes a delimiter between all elements in an iterable'''
-    it = iter(iterable)
-    yield next(it)
-    for x in it:
-        yield delimiter
-        yield x
-
-def write_result_to_file(data, filename, deep=True):
-    '''Helper function for writing results to file'''
-    if deep:
-        str_list = list(map(lambda x: ''.join(intersperse(map(str, x), '\t')), data))
-    else:
-        str_list = list(map(str, data))
-
-    with open('result/{}'.format(filename), 'w') as f:
-        f.write('\n'.join(str_list))
-    print("File has been written")
-
-def freq(word: str, tweet_words: list):
+def freq(word, tweet_words):
     '''Takes user x's total amount of tweets and a set of the
     union of words between user x and y, then returns the frequency'''
     return tweet_words.count(word)
 
-def sim_score(word_set: set, tweets_x: list, tweets_y: list):
+def sim_score(tweets_x, tweets_y):
     '''Calculates the similarity score betweeen two users'''
+    word_set = set().union(tweets_x, tweets_y)
     return reduce(
         lambda a, b: min(freq(a, tweets_x), freq(a, tweets_y))
         + min(freq(b, tweets_x), freq(b, tweets_y))
@@ -69,26 +53,54 @@ def get_user_words(rdd, user):
         raise ValueError("User doesn't exist in file")
     else:
         return(step
-               .map(lambda row: row[1])
-               .flatMap(lambda val: val.split())
+               .values()
+               .flatMap(lambda val: val.split()) #[user_words]
                .distinct()
                .collect())
 
-flatten = lambda l: [item for sublist in l for item in sublist]
+def get_union_words(rdd, queried_user_words, other_user):
+    return get_user_words(rdd, other_user).union(queried_user_words)
 
 # rdd = SC.parallelize([("meme", "he she tree lee fee ree"), ("nope", "top fope lee ree never ever"), ("scope", "tope rope lee tree she"), ("nope", "never ever lever top he")])
 
-def sim_usrs(rdd, user="tmj_bos_hr", k=0):
+def sim_usrs(rdd, user="tmj_bos_hr", k=3):
     '''Test'''
-    #user_words = get_user_words(rdd, user)
+    tweet_words = intern_rdd(rdd, user)
+    #queried_user_words = get_user_words(rdd, user)
+    #union_rows = union_rdd(rdd, user)
+    
+    return(rdd
+           .filter(lambda row: not row[0] == user)
+           .mapValues(lambda s: s.split()) #(user, [tweet_words])
+           #.reduceByKey(lambda a, b: a + b) #(user, [tweet_words])
+           .flatMapValues(identity) #(user, word)
+           .distinct()
+           .join(tweet_words) #(user, (word, [tweet_words]))
+           #.join(union_rows) #(user, ((word, [tweet_words]), {tweet_words_union_set}))
+           #.reduceByKey(lambda a, b:
+                       # min(freq(a[0][0], a[0][1]), freq(a[0][0], queried_user_words))
+                       # + min(freq(b[0][0], b[0][1]), freq(b[0][0], queried_user_words)))
+           .coalesce(1)
+           .saveAsTextFile("result/tweets"))
 
+def intern_rdd(rdd, user):
+    return(rdd
+           .filter(lambda row: not row[0] == user)
+           .mapValues(lambda s: s.split()) #(user, [tweet_words])
+           .reduceByKey(lambda a, b: a + b)) #(user, [tweet_words]))
+
+    
+def union_rdd(rdd, user):
+    '''Return rdd containing touples of user and union words for queried user'''
+    user_words = get_user_words(rdd, user)
+    
     return(rdd
            .filter(lambda row: not row[0] == user)
            .mapValues(lambda s: s.split())
-           #.reduceByKey(lambda x, y: x + y) #(user, [tweet_string])
-           #.mapValues(lambda t: (t, set(user_words).union(t), len(set(user_words).intersection(t)))) #(user, ([tweet_words], {tweet_words_union_set}, common_words_count))
-           #.mapValues(lambda t: (sim_score(t[1], user_words, t[0]), t[2])) #(user, (sim_score, common_words_count))
-           .collect())
+           .flatMapValues(identity) #(user, word)
+           .distinct()
+           .reduceByKey(lambda a, b: a + ' ' + b) #(user, tweet_words_string)
+           .mapValues(lambda l: l.split() + user_words)) #(user, [tweet_words_union_set])
 
 def similar_users(user, k, file_path, output_file):
     '''Writes similar users to file'''
