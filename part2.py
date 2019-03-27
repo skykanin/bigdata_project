5927be8d60cb1de95ecb9e1adc3bd0b0112206ba
skykanin <3789764+skykanin@users.noreply.github.com>
2019-03-27 16:45:11 +0100

    now runs as a standalone file

--- part2.py
+++ part2.py
@@ -2,36 +2,39 @@
 import sys
 from functools import reduce
 from pyspark import SparkConf, SparkContext
-from operator import concat
-
-CONF = SparkConf() \
-  .setAppName("main") \
-  .setMaster("local[*]") \
-  .setExecutorEnv('spark.driver.memory', '5g') \
-  .setExecutorEnv('spark.executor.memory', '3g')
-SC = SparkContext(conf=CONF)
-
-TWITTER_HEADERS = ["user", "tweet_text"]
-#TWITTER_RDD = SC.textFile("data/tweets.tsv").map(lambda line: split_and_map(line, TWITTER_HEADERS))
 
 def split_to_touple(line):
     '''Splits line into touple'''
     line_list = line.split('\t')
     return (line_list[0], line_list[1])
 
-TWITTER_RDD = SC.textFile("data/tweets.tsv").map(split_to_touple)
+# TWITTER_RDD = SC.textFile("data/tweets_small.tsv").map(split_to_touple)
 
 # Define helper functions for manipulating data and writing to file
-
-def split_and_map(line, header):
-    '''Splits string line and zips into a dict with appropriate headers'''
-    my_list = line.split('\t')
-    return dict(zip(header, my_list))
 
 def identity(a):
     '''Returns itself'''
     return a
 
+def intersperse(iterable, delimiter):
+    '''Intersperes a delimiter between all elements in an iterable'''
+    it = iter(iterable)
+    yield next(it)
+    for x in it:
+        yield delimiter
+        yield x
+        
+def write_result_to_file(data, filename, deep=True):
+    '''Helper function for writing results to file'''
+    if deep:
+        str_list = list(map(lambda x: ''.join(intersperse(map(str, x), '\t')), data))
+    else:
+        str_list = list(map(str, data))
+
+    with open('result/{}'.format(filename), 'w') as f:
+        f.write('\n'.join(str_list))
+    print("File has been written")
+    
 def freq(word, tweet_words):
     '''Takes user x's total amount of tweets and a set of the
     union of words between user x and y, then returns the frequency'''
@@ -46,11 +49,6 @@ def sim_score(tweets_x, tweets_y):
 
     return my_sum
 
-    '''return reduce(
-        lambda a, b: min(freq(a, tweets_x), freq(a, tweets_y))
-        + min(freq(b, tweets_x), freq(b, tweets_y))
-        , word_set)'''
-
 def get_user_words(rdd, user):
     '''Return list of all distinct user words from tweets'''
     step = rdd.filter(lambda row: row[0] == user)
@@ -63,82 +61,40 @@ def get_user_words(rdd, user):
                #.distinct()
                .collect())
 
-def get_union_words(rdd, queried_user_words, other_user):
-    return get_user_words(rdd, other_user).union(queried_user_words)
-
-# rdd = SC.parallelize([("meme", "he she tree lee fee ree"), ("nope", "top fope lee ree never ever"), ("scope", "tope rope lee tree she"), ("nope", "never ever lever top he")])
-
-def rmd_usrs(rdd, user="tmj_bos_hr", k=3):
-    '''Returns list of recommended users'''
-    tweet_words = intern_rdd(rdd, user)
-    union_rows = union_rdd(rdd, user)
-    queried_user_words = get_user_words(rdd, user)
-
-    return(rdd
-           .filter(lambda row: not row[0] == user)
-           .mapValues(lambda s: s.split()) #(user, [tweet_words])
-           #.reduceByKey(lambda a, b: a + b) #(user, [tweet_words])
-           .flatMapValues(identity) #(user, word)
-           .distinct()
-           .join(tweet_words) #(user, (word, [tweet_words]))
-           .join(union_rows) #(user, ((word, [tweet_words]), {tweet_words_union_set}))
-           .reduceByKey(lambda a, b:
-                        min(freq(a[0][0], a[0][1]), freq(a[0][0], queried_user_words))
-                        + min(freq(b[0][0], b[0][1]), freq(b[0][0], queried2_user_words)))
-           .take(k))
-           #.coalesce(1)
-           #.saveAsTextFile("result/tweets"))
-
-def rmd_usrs2(rdd=TWITTER_RDD, user="tmj_bos_hr", k=5):
+def rmd_usrs2(rdd, user="tmj_bos_hr", k=10):
     '''Returns list of recommended users'''
 
     queried_user_tweets = get_user_words(rdd, user) #[queried_user_tweet_words]
-    
-    return(rdd
-           .filter(lambda row: not row[0] == user)
-           .mapValues(lambda s: s.split())
-           .reduceByKey(lambda a, b: a + b) #(user, [tweet_words])
-           .map(lambda t: (sim_score(queried_user_tweets, t[1]), t[0])) #(sim_score, user)
-           #.sortByKey()
-           .reduceByKey(lambda a, b: a + ' ' + b)
-           .mapValues(lambda s: sorted(s.split())) #(sim_score, [users])
-           .flatMapValues(lambda v: v) # (sim_score, user)
-           .takeOrdered(k, key=lambda t: -t[0]))
 
-def intern_rdd(rdd, user):
-    '''Return touple of user and list of tweet words'''
-    return(rdd
-           .filter(lambda row: not row[0] == user)
-           .mapValues(lambda s: s.split()) #(user, [tweet_words])
-           .reduceByKey(lambda a, b: a + b)) #(user, [tweet_words]))
+    result = (rdd
+              .filter(lambda row: not row[0] == user)
+              .mapValues(lambda s: s.split())
+              .reduceByKey(lambda a, b: a + b) #(user, [tweet_words])
+              .map(lambda t: (sim_score(queried_user_tweets, t[1]), t[0])) #(sim_score, user)
+              .reduceByKey(lambda a, b: a + ' ' + b)
+              .mapValues(lambda s: sorted(s.split())) #(sim_score, [users])
+              .flatMapValues(identity) # (sim_score, user)
+              .takeOrdered(k, key=lambda t: -t[0]))
 
-
-def union_rdd(rdd, user):
-    '''Return rdd containing touples of user and union words for queried user'''
-    user_words = get_user_words(rdd, user)
-
-    return(rdd
-           .filter(lambda row: not row[0] == user)
-           .mapValues(lambda s: s.split())
-           .flatMapValues(identity) #(user, word)
-           .distinct()
-           .reduceByKey(lambda a, b: a + ' ' + b) #(user, tweet_words_string)
-           .mapValues(lambda l: l.split() + user_words)) #(user, [tweet_words_union_set])
-
-def recommended_users(user, k, file_path, output_file):
-    '''Writes similar users to file'''
-    conf = SparkConf().setAppName("main").setMaster("local[*]")
-    sc = SparkContext(conf=conf)
-
-    twitter_headers = ["user", "tweet_text"]
-    twitter_rdd = sc.textFile(file_path).map(lambda line: split_and_map(line, twitter_headers))
+    return map(lambda t: (t[1], t[0]), result)
 
 def main(argv):
     '''Main function'''
-    print(argv)
+    CONF = SparkConf() \
+      .setAppName("main") \
+      .setMaster("local[*]") \
+      .setExecutorEnv('spark.driver.memory', '5g') \
+      .setExecutorEnv('spark.executor.memory', '3g')
+    SC = SparkContext(conf=CONF)
+    
     my_dict = {argv[1]:argv[2], argv[3]:argv[4], argv[5]:argv[6], argv[7]:argv[8]}
-    #rmd_usrs2(my_dict["-user"], my_dict=["-k"], my_dict["-file"], my_dict["-output"])
+    
+    my_rdd = SC.textFile(my_dict["-file"]).map(split_to_touple)
+        
+    result = rmd_usrs2(rdd=my_rdd, user=my_dict["-user"], k=int(my_dict["-k"]))
+    
+    write_result_to_file(result, my_dict["-output"])
 
-'''if __name__ == "__main__":
-    main(sys.argv)'''
+if __name__ == "__main__":
+    main(sys.argv)
     
